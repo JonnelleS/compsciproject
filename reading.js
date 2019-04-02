4f505d1d2e819e6bf39baaeb7f47f2a7f409b6ef
Jonnelle Sayles <jonnellesayles@gmail.com>
2019-04-02 19:31:34 -0400

    Update reading.js

--- reading.js
+++ reading.js
@@ -3,5 +3,5 @@ $("#submit").click(function() {
     var input = $("#adjecctive").val();
     var input = $("#day").val();
     
-    $("p").append("Hello " + $("#name").val() + ", isn't it a "+ $("#adjective").val()+ " " + $("#day").val()+ "? I think it would be cool to "+ $("#verb").val() + " at the patk"  );
+    $("p").append("Hello " + $("#name").val() + ", isn't it a "+ $("#adjective").val()+ " " + $("#day").val()+ "?" );
 });
