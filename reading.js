354cb9d977c5a3dc0cd7a176a4e7a2b63fb7d827
Jonnelle Sayles <jonnellesayles@gmail.com>
2019-04-03 00:26:45 -0400

    Delete reading.js

--- reading.js
+++
@@ -1,7 +0,0 @@
-$("#submit").click(function() {
-    var input = $("#name").val();
-    var input = $("#adjecctive").val();
-    var input = $("#day").val();
-    
-    $("p").append("Hello " + $("#name").val() + ", isn't it a "+ $("#adjective").val()+ " " + $("#day").val()+ "?" );
-});
