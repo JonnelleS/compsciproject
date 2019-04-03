819a0bacfe7d046cb0402a6c87ed3d1cdfeae333
Jonnelle Sayles <jonnellesayles@gmail.com>
2019-04-03 00:28:26 -0400

    Update madlib.js

--- madlib.js
+++ madlib.js
@@ -1,7 +1,7 @@
 $("#submit").click(function() {
     var input = $("#name").val();
-    var input = $("#adjecctive").val();
+    var input = $("#adj").val();
     var input = $("#day").val();
     
-    $("p").append("Hello " + $("#name").val() + ", isn't it a "+ $("#adjective").val()+ " " + $("#day").val()+ "? I think it would be cool to "+ $("#verb").val() + " at the patk"  );
+    $("p").append("Hello " + $("#name1").val() + ", isn't it a "+ $("#adj").val()+ " " + $("#weekday").val() + "? Maybe " + $("#name2").val() + " would want to go to " + $("#their").val() + $("#place").val() + " to play " + $("#sport").val() + " Don't you think that would be " +  $("#descriptor").val());
 });
